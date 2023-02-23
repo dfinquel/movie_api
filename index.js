e24a061555bc0124bc6108f2090996496e09367b
dfinquel <119966119+dfinquel@users.noreply.github.com>
2023-02-23 14:10:02 +0100

    Use express.static to serve documentation.html from public folder

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+/*Below, used express.static to serve the “documentation.html” file from the public folder.*/
+app.use(express.static('public'));
+
 /*Below, created an Express GET route located at the endpoint “/” that returns a textual response.*/
 app.get('/', (req, res) => {
     res.send('Welcome to myFlix!');
