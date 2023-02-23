1a66e3f00ac01f048271570bae752f7d75299b35
dfinquel <119966119+dfinquel@users.noreply.github.com>
2023-02-23 13:56:37 +0100

    Create Express GET route at '/' that returns textual response

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+/*Below, created an Express GET route located at the endpoint “/” that returns a textual response.*/
+app.get('/', (req, res) => {
+    res.send('Welcome to myFlix!');
+  });
+
 /*Below, created an Express GET route located at the endpoint “/movies” that returns a JSON object containing data about topMovies.*/
 app.get('/movies', (req, res) => {
     res.json(topMovies);
