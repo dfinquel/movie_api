2b90c200f6a65289e85bf4ad5b3a08bb0eb48028
dfinquel <119966119+dfinquel@users.noreply.github.com>
2023-02-23 14:13:28 +0100

    Add function to listen for requests on port 8080

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ app.use(express.static('public'));
 /*Below, created an Express GET route located at the endpoint “/” that returns a textual response.*/
 app.get('/', (req, res) => {
     res.send('Welcome to myFlix!');
-  });
+});
 
 /*Below, created an Express GET route located at the endpoint “/movies” that returns a JSON object containing data about topMovies.*/
 app.get('/movies', (req, res) => {
     res.json(topMovies);
-  });
+});
+
+/*Below, added function to listen for requests on port 8080.*/
+app.listen(8080, () => {
+    console.log('The myFlix app is listening on port 8080.');
+});
