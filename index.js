5e059af3e3dd95f9491d2139f1b631b8732a4fe5
dfinquel <119966119+dfinquel@users.noreply.github.com>
2023-02-23 13:53:41 +0100

    Create Express GET route at '/movies' that returns topMovies

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const app = express();
+
+/*Below, created an Express GET route located at the endpoint “/movies” that returns a JSON object containing data about topMovies.*/
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
